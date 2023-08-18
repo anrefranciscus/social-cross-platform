9d4318a10f1abf9534a85b163a05f2ac9e9460a7
Anre Franciscus <anrefranciscus@gmail.com>
2023-08-18 23:28:36 +0700

    adding header and footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,42 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header
+      class="bg-white text-grey-10"
+      bordered
+    >
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Warsada App
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
+    <q-footer
+      class="bg-white small-screen-only"
       bordered
     >
-      <q-list>
-        <q-item-label
-          header
+      <q-toolbar class="text-grey-10 flex">
+        <q-tabs
+          v-model="tab"
+          active-color="primary"
+          indicator-color="transparent"
+          class="full-width"
         >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+          <q-route-tab
+            to="/"
+            name="home"
+            icon="eva-home-outline"
+            label="Home"
+          />
+          <q-route-tab
+            to="/camera"
+            name="camera"
+            icon="eva-camera-outline"
+            label="Camera"
+          />
+        </q-tabs>
+      </q-toolbar>
+    </q-footer>
 
     <q-page-container>
       <router-view />
@@ -46,71 +45,24 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import {rule} from "postcss";
 
-export default defineComponent({
+export default  {
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
+  data () {
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      tab: null
     }
   }
-})
+}
 </script>
+
+<style lang="sass">
+.q-toolbar__title
+  text-align: center
+  font-size: 30px
+.q-footer
+  .q-tab__icon
+    font-size: 50px
+</style>
---
+++ src/pages/PageCamera.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "PageCamera"
+}
+</script>
+
+<style scoped>
+
+</style>
